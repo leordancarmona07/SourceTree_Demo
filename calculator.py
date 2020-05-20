7870151ba77f3680ceff197c05020e2688c2e2be
jjjump199x <jjjump@github>
2020-05-20 11:36:36 +0800

    Added_require Inputs

--- calculator.py
+++ calculator.py
@@ -1,1 +1,7 @@
 print("This is a calculatorS")
+
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
+
+print("First Number: " + str(num1))
+print("Second Number: " + str(num2))
