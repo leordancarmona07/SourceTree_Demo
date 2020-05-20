b6c1157975bd78192f8ceb6fcd61c4a33d068813
jjjump199x <jjjump@github>
2020-05-20 11:43:36 +0800

    Basic Calculator With Basic Operator

--- calculator.py
+++ calculator.py
@@ -3,5 +3,10 @@ print("This is a calculatorS")
 num1 = int(input("First Number: "))
 num2 = int(input("Second Number: "))
 
-print("First Number: " + str(num1))
-print("Second Number: " + str(num2))
+# print("First Number: " + str(num1))
+# print("Second Number: " + str(num2))
+
+operator = input("Enter Operator: ")
+
+if (operator == "+"):
+    print ("Result is equal to :" + str(num1 + num2))
